d5bc88f78af2fc37953e0b72a8ae01aad80153cd
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2020-07-21 15:41:38 -0700

    created serial number generate and reset methods on the class SerialGenerator

--- serial.py
+++ serial.py
@@ -17,3 +17,21 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__ (self, start=100):
+        """Creates serial number and original number. Original number used to reset serial number"""
+        self.original_start = start
+        self.serial_number = start
+    
+    def __repr__(self):
+        return f"<Serial Generator start={self.original_start}>"
+
+    
+    def generate(self):
+        """Generates a serial number, starting with the value for the start argument or the default start value of 100"""
+        self.serial_number += 1
+        return self.serial_number - 1
+
+    def reset(self):
+        """Resets the serial number to the original starting number"""
+        self.serial_number = self.original_start
